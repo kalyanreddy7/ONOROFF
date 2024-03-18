9325488cbc58b84fdf5893f59ad6c0ba1af7e9c7
kalyanreddy7 <163332773+kalyanreddy7@users.noreply.github.com>
2024-03-18 16:08:58 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,7 @@
+function getoff(){
+
+   document.getElementById("l1").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8jpdHR_pFaoP2Vudl7k-46mSK7I3shuCvFw&usqp=CAU";
+}
+function geton(){
+    document.getElementById("l1").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1ytqywxxINY_RpV1AqxTf7I2Emo9my3So4w&usqp=CAU";
+ }
